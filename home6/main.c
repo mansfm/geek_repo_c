72aa049909adfd37985c688e9d5267279d2ffbc7
Mikhail Mansurov <mansfm@yandex.ru>
2021-10-11 23:04:16 +0000

    uppercase and lowercase

--- home6/main.c
+++ home6/main.c
@@ -38,8 +38,12 @@ void counting_characters(void)
 	char c;
 	int total = 0;
 	int digits = 0;
+	int uppercase = 0;
+	int lowercase = 0;
 	int spaces = 0;
 	int percent_of_digits = 0;
+	int percent_of_uppercase = 0;
+	int percent_of_lowercase = 0;
 	int percent_of_spaces = 0;
 
 	while((c = getchar()) != '\n') {
@@ -51,14 +55,23 @@ void counting_characters(void)
 			digits++; //количество цифр
         	}
 
+		if ('a' <= c && c <= 'z') {
+			uppercase++;
+		}
+
+		if ('A' <= c && c <= 'Z') {
+			lowercase++;
+		}
+		
 		if (c == ' ') {
 			spaces++; //количество пробелов
 		}
     	}
 
-	printf("Total %d\n", total);
-	
+	printf("Total %d\n", total); //вывод общего количества символов
 	printf("Digits %d (%d%%)\n", digits, percent_of_digits = (float)digits/(float)total*100); //вывод количества цифр и процента цифр от общего количества символов
+        printf("Uppercase %d (%d%%)\n", uppercase, percent_of_uppercase = (float)uppercase/(float)total*100); //вывод количества букв в верхнем регистре и процент от общего количества символов
+        printf("Lowercase %d (%d%%)\n", lowercase, percent_of_lowercase = (float)lowercase/(float)total*100); //вывод количества букв в нижнем регистре и процент от общего количества символов
 	printf("Spaces %d (%d%%)\n", spaces, percent_of_spaces = (float)spaces/(float)total*100); //вывод количества пробелов и процент пробелов от общего количества символов
 
 }
