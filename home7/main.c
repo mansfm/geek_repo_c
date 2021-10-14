e81beb56d5c3d1c7cb354884f753b6cfa5fac3c6
Mikhail Mansurov <mansfm@yandex.ru>
2021-10-14 23:55:48 +0000

    first

---
+++ home7/main.c
@@ -0,0 +1,50 @@
+/*
+Домашнее задание:
+
+Программа "стрельба в мишень"
+
+Пользователь вводит с клавиатуры 3 числа типа float(X, Y, Radius)
+
+Первая функция вида
+bool get_user_input(float *X, float *Y, float *R)
+считывает с клавиатуры значения координат X,Y и радиуса, и проводит валидацию входных значений.(количество успешных преобразований от функции scanf, неотрицательный и ненулевой радиус и так далее)
+Если происходит ошибка - функция возвращает false. В этом случае программа с ошибкой завершается.
+
+Вторая функция вида
+bool is_target_hit(float X, float Y, float R)
+проверяет, попал ли пользователь в мишень. Формула X*X + Y*Y < R*R
+Результат выводится на экран(printf)
+*/
+
+
+#include <stdio.h>
+#include <stdbool.h>
+
+bool get_user_input(int *a, int *b);
+
+
+int main(void) 
+{
+	int a = 5, b = 10;
+
+	bool ret = get_user_input(&a, &b);
+	if (ret == false) {
+		printf("Error!\n");
+		return -1;
+	}
+	
+	return 0;
+}
+
+
+bool get_user_input(int *a, int *b)
+{
+	int retcode;
+
+	retcode = scanf("%d%d", a, b);
+	if (retcode == 2) {
+        	return true;
+	} else {
+		return false;
+	}
+}
