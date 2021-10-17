421656d9776312d8bda9fc438ff5090b59784c99
Mikhail Mansurov <mansfm@yandex.ru>
2021-10-17 00:43:32 +0000

    test3

--- home7/main.c
+++ home7/main.c
@@ -20,44 +20,52 @@ bool is_target_hit(float X, float Y, float R)
 #include <stdio.h>
 #include <stdbool.h>
 
-bool get_user_input(int *a, int *b);
-void swap (int *a, int *b);
+//bool get_user_input(int *a, int *b);
+void swap (float *X, float *Y, float *R);
+
+
+bool get_user_input(float *X, float *Y, float *R);
+
 
 
 int main(void) 
 {
-	int a = 5, b = 10;
+	float X = 1, Y = 2, R = 3;
 
-	int *p_a = &a;
-	int *p_b = &b;
+	float *p_x = &X;
+	float *p_y = &Y;
+	float *p_r = &R;
 
-	bool ret = get_user_input(&a, &b);
+	bool ret = get_user_input(&X, &Y, &R);
 	if (ret == false) {
 		printf("Error!\n");
 		return -1;
 	}
 
-	swap(p_a, p_b);
-	printf("%d %d\n", a, b);
+	swap(p_x, p_y, p_r);
+	printf("%f %f %f\n", X, Y, R);
 	
 	return 0;
 }
 
 
-void swap(int *a, int *b)
+
+void swap(float *X, float *Y, float *R)
 {
-	int tmp = *a;
-	*a = *b;
-	*b = tmp;
+	float tmp = *X;
+	*X = *Y;
+	*Y = tmp;
+	*R = *R;
 }
 
 
-bool get_user_input(int *a, int *b)
+
+bool get_user_input(float *X, float *Y, float *R)
 {
 	int retcode;
 
-	retcode = scanf("%d%d", a, b);
-	if (retcode == 2) {
+	retcode = scanf("%f%f%f", X, Y, R);
+	if (retcode == 3) {
         	return true;
 	} else {
 		return false;
